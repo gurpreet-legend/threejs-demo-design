91c8038abe2abe71cfd0b8fad4440c6206c9791b
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-11-25 01:51:41 +0530

    added texture

--- src/script.js
+++ src/script.js
@@ -3,6 +3,11 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+//Texture loader
+const textureLoader = new THREE.TextureLoader();
+
+const normalTexture = textureLoader.load('/textures/NormalMap.png')
+
 // Debug
 const gui = new dat.GUI()
 
@@ -20,6 +25,7 @@ const geometry = new THREE.SphereBufferGeometry( .5, 64, 64);
 const material = new THREE.MeshStandardMaterial()
 material.metalness = 0.7
 material.roughness = 0.2
+material.normalMap = normalTexture;
 material.color = new THREE.Color(0x292929)
 
 // Mesh
